6c6bb11ad5631518b5664e3dc141d13f3dca193f
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2024-11-23 11:59:28 -0300

    build: spring initial files

---
+++ src/main/java/br/edu/ifpb/photobean/PhotobeanApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.photobean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotobeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotobeanApplication.class, args);
+	}
+
+}
