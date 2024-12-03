db3544fabae8f4959fb937b0a3f8d0053791c663
CaioBernardelli <caiojp2013@hotmail.com>
2024-12-03 13:58:32 -0300

    model

---
+++ src/main/java/br/edu/ifpb/photobean/model/Follow.java
@@ -0,0 +1,32 @@
+package br.edu.ifpb.photobean.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "follow")
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne //Um registro de "seguir" está vinculado ao fotógrafo que está seguindo.
+    @JoinColumn(name = "follower_id")
+    private Photographer follower;
+
+
+
+    @ManyToOne //Um registro de "seguir" está vinculado ao fotógrafo que está seguindo.
+    @JoinColumn(name = "follower_id")
+    private Photographer followee;//Um registro de "seguir" está vinculado ao fotógrafo que está sendo seguido.
+
+
+}
