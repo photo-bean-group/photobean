a1419a8d190ecfc75d312a646c90a6e70ea20754
Arthur Lyra Miranda <arthurlyramiranda1999@gmail.com>
2025-01-13 23:34:48 -0300

    feat: modificação dos arquivos para cadastramento do usuario.

--- src/main/java/br/edu/ifpb/photobean/controller/CadastroController.java
+++ src/main/java/br/edu/ifpb/photobean/controller/CadastroController.java
@@ -1,12 +1,13 @@
 package br.edu.ifpb.photobean.controller;
 
 import br.edu.ifpb.photobean.Service.Servico_Photographer;
+import br.edu.ifpb.photobean.model.Photo;
 import br.edu.ifpb.photobean.model.Photographer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -16,13 +17,20 @@ public class CadastroController {
     @Autowired
     private Servico_Photographer servico_Photographer;
 
+    @RequestMapping("/form")
+    public ModelAndView getForm(ModelAndView modelAndView) {
+        modelAndView.setViewName("cadastro/form");
+        modelAndView.addObject("photographer", new Photographer());
+        return modelAndView;
+    }
+
     @PostMapping("/Criar")
     public String CriarCadastro(Photographer photographer, RedirectAttributes redirectAttributes) {
         try{
             Photographer savedPhotographer = servico_Photographer.savePhotographer(photographer);
             //Adiciona mensagem de sucesso para a próxima requisição
             redirectAttributes.addFlashAttribute("mensagemSucesso", "Cadastrado com sucesso!");
-            return "redirect:/Cadastro/Criar";
+            return "redirect:/Cadastro/Criado";
         }catch (IllegalArgumentException e){
             //Adiciona mensagem de erro para a próxima requisição
             redirectAttributes.addFlashAttribute("mensagemErro", e.getMessage());
@@ -30,15 +38,15 @@ public class CadastroController {
         }
     }
 
-  //  @GetMapping("/Criar")
-   // public String MostrarCadastroForms(Model model) {
-   //     if(!model.containsAttribute("photographer")){
-   //         model.addAttribute("photographer", new Photographer());
-   //     }
-   //     return "CadastroForms"; //Nome do template para o formulário
-   // }
-   // @GetMapping("/sucesso")
-   // public String showSuccessPage() {
-   //     return "sucesso"; // Nome do template para a página de sucesso
-   // }
+    @GetMapping("/Criado")
+    public String MostrarCadastroForms(Model model) {
+        if(!model.containsAttribute("photographer")){
+            model.addAttribute("photographer", new Photographer());
+        }
+        return "cadastro/form"; //Nome do template para o formulário
+    }
+    @GetMapping("/sucesso")
+    public String showSuccessPage() {
+        return "sucesso"; // Nome do template para a página de sucesso
+    }
 }
