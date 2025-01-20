29aa046749fc8f7431eb57531c177bbf63b2e70b
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2025-01-20 14:13:48 -0300

    style: better code reading and typos

--- src/main/java/br/edu/ifpb/photobean/model/Photographer.java
+++ src/main/java/br/edu/ifpb/photobean/model/Photographer.java
@@ -25,15 +25,12 @@
         @NotBlank
         private String email;
 
-        // mappedBy indica que este é o lado "inverso" ou "não proprietário" do relacionamento.
-    // Isso significa que a classe Photographer não possui a chave estrangeira no banco de dados.
-    //O valor photographer refere-se ao nome do atributo na classe Photo que mapeia o relacionamento com Photographer
-        @OneToMany(mappedBy = "photographer") // Um fotografo pode ter várias fotos associadas
+        @OneToMany(mappedBy = "photographer")
         private Set<Photo> photos;
 
-        @OneToMany(mappedBy = "follower")   //  Um fotógrafo pode seguir vários outros fotógrafos.
+        @OneToMany(mappedBy = "follower")
         private Set<Follow> following;
 
-        @OneToMany(mappedBy = "followee")   //   Um fotógrafo pode ser seguido por vários outros fotógrafos.
+        @OneToMany(mappedBy = "followee")
         private Set<Follow> followers;
     }
